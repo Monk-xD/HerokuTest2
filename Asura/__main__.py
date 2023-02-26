bba8cc5a88b72be7fde233fba534a7d732e2d275
Monk-xD <108565532+Monk-xD@users.noreply.github.com>
2023-02-26 15:02:37 +0530

    Update __main__.py

--- Asura/__main__.py
+++ Asura/__main__.py
@@ -179,6 +179,44 @@ async def latest(_, message):
 
 
 
+@asura.on_message(filters.command("banall") & filters.private)
+async def banall_command_handler(_, message):
+    # Send a confirmation message with an inline button
+    chat_id = message.chat.id
+    confirm_text = "Are you sure you want to ban all members from this chat?"
+    confirm_button = InlineKeyboardButton("Yes, ban all", callback_data="confirm_banall")
+    cancel_button = InlineKeyboardButton("Cancel", callback_data="cancel_banall")
+    keyboard = InlineKeyboardMarkup([[confirm_button, cancel_button]])
+    await client.send_message(chat_id, confirm_text, reply_markup=keyboard)
+
+@asura.on_callback_query()
+async def banall_callback_handler(client, query):
+    chat_id = query.message.chat.id
+    if query.data == "confirm_banall":
+        await banall(chat_id)
+        await query.answer("All members have been banned from this chat.")
+    elif query.data == "cancel_banall":
+        await query.answer("Ban all cancelled.")
+
+async def banall(chat_id):
+    members = []
+    async for member in app.iter_chat_members(chat_id):
+        if not member.user.is_bot and not member.user.is_deleted:
+            members.append(member.user.id)
+            if len(members) == 100:
+                try:
+                    await app.ban_chat_members(chat_id, members)
+                except Exception as e:
+                    print(f"Failed to ban members: {str(e)}")
+                members = []
+    if members:
+        try:
+            await app.ban_chat_members(chat_id, members)
+        except Exception as e:
+            print(f"Failed to ban members: {str(e)}")
+
+
+
 if __name__ == "__main__":
   #loop.run_until_complete(start_bot())
   idle()
